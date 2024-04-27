51b24daf749e4f3f5b9c8532f12f254f0eac6785
Đỗ Hùng <dohung2ka3@gmail.com>
2024-04-27 18:52:42 +0700

    feat: made process and totalTime logic of sidebar

--- src/components/Courses/Grammar/GrammarLessonCard.tsx
+++ src/components/Courses/Grammar/GrammarLessonCard.tsx
@@ -1,10 +1,7 @@
 // ##################################
 // #       IMPORT Components
 // ##################################
-import {
-    useGetAllUnitLessonsByCourseIdQuery,
-    useGetAllLessonsByCourseIdQuery,
-} from '@store/api/courseApi';
+import { useGetAllUnitLessonsByCourseIdQuery, useGetAllLessonsByCourseIdQuery } from '@store/api/courseApi';
 import { LessonType, UnitLessonType } from 'types/api-types';
 
 // ##################################
@@ -15,13 +12,9 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { useNavigate } from 'react-router-dom';
 import CreateIcon from '@mui/icons-material/Create';
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
-const headerSidebar: React.FC<{ title: string; process: string; totalTime: string }> = ({
-    title,
-    process,
-    totalTime,
-}) => {
+const headerSidebar: React.FC<{ title: string; process: string; totalTime: string }> = ({ title, process, totalTime }) => {
     return (
         <div>
             <h4 className="font-body text-base font-bold text-textCustom">{title}</h4>
@@ -35,19 +28,84 @@ const headerSidebar: React.FC<{ title: string; process: string; totalTime: strin
     );
 };
 
-const GrammarLessonCard: React.FC<{ handleToggleLesson: () => void }> = ({
-    handleToggleLesson,
-}) => {
+const GrammarLessonCard: React.FC<{ handleToggleLesson: () => void }> = ({ handleToggleLesson }) => {
     //  Get id from url
     const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
     const { id } = useParams();
+    const unitLessonIdUrl = searchParams.get('id');
+    const { data, isLoading } = useGetAllLessonsByCourseIdQuery(id || '');
+    const { data: dataUnitLesson } = useGetAllUnitLessonsByCourseIdQuery(id || '');
+    // ##########################
+    // #    STATE MANAGEMENT    #
+    // ##########################
+
+    // ##########################
+    // #  FUNCTION MANAGEMENT   #
+    // ##########################
     const handleRedirect = (unitLessonId: string) => {
         navigate(`/grammar/${id}?id=${unitLessonId}`);
         handleToggleLesson();
     };
 
-    const { data, isLoading } = useGetAllLessonsByCourseIdQuery(id || '');
-    const { data: dataUnitLesson } = useGetAllUnitLessonsByCourseIdQuery(id || '');
+    const handleGetProcess = (lessonId: string) => {
+        let process = 0;
+
+        if (dataUnitLesson) {
+            dataUnitLesson.unitLessons.forEach((unitLesson: UnitLessonType) => {
+                if (unitLesson.lesson === lessonId) {
+                    process += 1;
+                }
+            });
+        }
+
+        return `0/${process}`;
+    };
+
+    const handleGetTotalTime = (lessonId: string) => {
+        let totalHours = 0;
+        let totalMinutes = 0;
+        let totalSeconds = 0;
+
+        if (dataUnitLesson) {
+            dataUnitLesson.unitLessons.forEach((unitLesson: UnitLessonType) => {
+                if (unitLesson.lesson === lessonId) {
+                    const timeParts = unitLesson.time.split(':');
+                    const hours = parseInt(timeParts[0]);
+                    const minutes = parseInt(timeParts[1]);
+                    const seconds = parseInt(timeParts[2]);
+
+                    totalHours += hours;
+                    totalMinutes += minutes;
+                    totalSeconds += seconds;
+                }
+            });
+
+            // Chuyển đổi phút và giây thành giờ nếu cần
+            totalMinutes += Math.floor(totalSeconds / 60);
+            totalSeconds %= 60;
+            totalHours += Math.floor(totalMinutes / 60);
+            totalMinutes %= 60;
+        }
+
+        return `${totalHours.toString().padStart(2, '0')}:${totalMinutes.toString().padStart(2, '0')}:${totalSeconds
+            .toString()
+            .padStart(2, '0')}`;
+
+        /**
+         * totalHours.toString().padStart(2, '0'):
+         *    - Đây là cách chuyển đổi số giờ thành chuỗi và đảm bảo rằng chuỗi này luôn có ít nhất 2 ký tự.
+         *    - .toString(): Chuyển đổi số giờ (totalHours) thành chuỗi.
+         *    - .padStart(2, '0'): Nếu chuỗi có ít hơn 2 ký tự, thêm các ký tự '0' vào đầu chuỗi cho đến khi có đủ 2 ký tự.
+         * :: Đây là ký tự ngăn cách giữa giờ, phút và giây trong định dạng thời gian HH:mm:ss.
+         * totalMinutes.toString().padStart(2, '0'):
+         *    - Tương tự như trên, đây là cách chuyển đổi số phút thành chuỗi và đảm bảo chuỗi này luôn có ít nhất 2 ký tự.
+         * totalSeconds.toString().padStart(2, '0'):
+         *    - Cũng tương tự như trên, đây là cách chuyển đổi số giây thành chuỗi và đảm bảo chuỗi này luôn có ít nhất 2 ký tự.
+         * ****: Dấu `` là một phần của cú pháp "template literals" trong JavaScript, cho phép chèn biến vào trong chuỗi một cách dễ đọc và dễ hiểu.
+         * Trong trường hợp này, nó cho phép chèn các giá trị của totalHours, totalMinutes, và totalSeconds vào trong chuỗi một cách dễ dàng.
+         */
+    };
 
     return (
         <Accordion>
@@ -56,8 +114,8 @@ const GrammarLessonCard: React.FC<{ handleToggleLesson: () => void }> = ({
                     <Accordion.Panel
                         header={headerSidebar({
                             title: lesson.name,
-                            process: '20/20',
-                            totalTime: '5:00:00',
+                            process: handleGetProcess(lesson._id),
+                            totalTime: handleGetTotalTime(lesson._id),
                         })}
                         key={lesson._id}
                     >
@@ -67,24 +125,20 @@ const GrammarLessonCard: React.FC<{ handleToggleLesson: () => void }> = ({
                                     unitLesson.lesson === lesson._id ? (
                                         <li
                                             className={`flex items-center justify-between 
-                                            ${unitLesson._id === id ? 'bg-bgHoverGrayDark' : ''} 
+                                            ${unitLesson._id === unitLessonIdUrl ? 'bg-bgHoverGrayDark' : ''} 
                                             cursor-pointer rounded-lg p-2 transition-all duration-300 hover:bg-bgHoverGrayDark`}
                                             key={unitLesson._id}
                                             onClick={() => handleRedirect(unitLesson._id)}
                                         >
                                             <div>
-                                                <h4 className="mb-2 text-sm font-semibold text-textCustom">
-                                                    {unitLesson.title}
-                                                </h4>
+                                                <h4 className="mb-2 text-sm font-semibold text-textCustom">{unitLesson.title}</h4>
                                                 <div className="flex items-center gap-2">
                                                     {unitLesson.icon === 'videoLecture' ? (
                                                         <PlayCircleFilledIcon className="text-xs text-orange-400" />
                                                     ) : (
                                                         <CreateIcon className="text-xs text-orange-400" />
                                                     )}
-                                                    <p className="text-xs text-textCustom">
-                                                        {unitLesson.time}
-                                                    </p>
+                                                    <p className="text-xs text-textCustom">{unitLesson.time}</p>
                                                 </div>
                                             </div>
 
