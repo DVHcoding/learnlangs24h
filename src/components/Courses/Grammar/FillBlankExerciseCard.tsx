c5414e7d4222ad52153d5a4060fbcc50000dfb7d
Đỗ Hùng <dohung2ka3@gmail.com>
2024-04-28 23:15:55 +0700

    feat: made nextUnitLesson logic

--- src/components/Courses/Grammar/FillBlankExerciseCard.tsx
+++ src/components/Courses/Grammar/FillBlankExerciseCard.tsx
@@ -1,21 +1,31 @@
 // ##########################
 // #      IMPORT NPM        #
 // ##########################
-import { useSearchParams } from 'react-router-dom';
-import { useState, Fragment } from 'react';
+import { useSearchParams, useNavigate, useParams } from 'react-router-dom';
+import { useState, Fragment, useEffect } from 'react';
 import { X, Check } from 'lucide-react';
 import { Empty } from 'antd';
+import dayjs from 'dayjs';
 
 // ##########################
 // #    IMPORT Components   #
 // ##########################
-import { useGetFillBlankExerciseQuery } from '@store/api/courseApi';
-import { QuestionType } from 'types/api-types';
+import {
+    useGetAllLessonsByCourseIdQuery,
+    useGetFillBlankExerciseQuery,
+    useGetUnitLessonByIdQuery,
+    useGetAllUnitLessonsByLessonIdQuery,
+} from '@store/api/courseApi';
+import { LessonType, QuestionType, UnitLessonType } from 'types/api-types';
 
 const FillBlankExerciseCard: React.FC = () => {
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
-    const id = searchParams.get('id');
+    const id = searchParams.get('id'); // ?id=.....
+    const { id: courseId } = useParams();
     const { data: fillBlankExerciseData, isLoading: fillBlankExerciseLoading } = useGetFillBlankExerciseQuery(id || '');
+    const { data: lessons, isLoading: getAllLessonsLoading } = useGetAllLessonsByCourseIdQuery(courseId || '');
+    const { data: unitLesson, isLoading: getUnitLessonByIdLoading } = useGetUnitLessonByIdQuery(id || '');
 
     // ##########################
     // #    STATE MANAGEMENT    #
@@ -24,10 +34,14 @@ const FillBlankExerciseCard: React.FC = () => {
     const [results, setResults] = useState<boolean>(false);
     const [allCorrect, setAllCorrect] = useState<boolean>(false);
 
+    const [lessonId, setLessonId] = useState<string>('');
     // ##########################
     // #  FUNCTION MANAGEMENT   #
     // ##########################
 
+    const { data: unitLessons } = useGetAllUnitLessonsByLessonIdQuery(lessonId || '');
+
+    // # Hàm lấy các giá trị người dùng nhập vào và kiểm tra xem đúng không
     const handleSetFillBlankAnswers: (e: React.ChangeEvent<HTMLInputElement>, index: number, questionIndex: number) => void = (
         e,
         index,
@@ -45,7 +59,8 @@ const FillBlankExerciseCard: React.FC = () => {
         setResults(false);
     };
 
-    const handleChecking = () => {
+    // # Hàm check đáp án khi nhấn vào nút kiểm tra
+    const handleChecking: () => void = () => {
         setResults(true);
 
         if (!fillBlankExerciseLoading && fillBlankExerciseData?.fillBlankExercise) {
@@ -75,48 +90,84 @@ const FillBlankExerciseCard: React.FC = () => {
         }
     };
 
+    // # Hàm chuyển sang bài học tiếp theo
+    const handleNextUnitLesson: () => void = () => {
+        if (!getAllLessonsLoading && lessons?.success && !getUnitLessonByIdLoading && unitLesson?.success) {
+            if (unitLessons?.success) {
+                const getUnitLessonIndex = unitLessons.unitLessons.findIndex((unitLesson: UnitLessonType) => {
+                    return unitLesson._id === id;
+                });
+
+                const nextUnitLessonId = unitLessons.unitLessons[getUnitLessonIndex + 1]?._id;
+
+                if (nextUnitLessonId) {
+                    navigate(`?id=${nextUnitLessonId}`);
+                }
+
+                // const getLessonIndex = lessons.lessons.findIndex((lesson: LessonType) => {
+                //     return lesson._id === lessonId;
+                // });
+                // const nextLessonId = lessons.lessons[getLessonIndex + 1]._id;
+                // setLessonId(nextLessonId);
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (!getAllLessonsLoading && lessons?.success && !getUnitLessonByIdLoading && unitLesson?.success) {
+            const lessonId = unitLesson.unitLesson.lesson;
+            setLessonId(lessonId);
+        }
+    }, []);
+
     return (
         <div>
-            <h2 className="font-title text-xl font-bold text-textCustom">Thực hành thì Hiện đơn</h2>
-            <p className="text-base text-textCustom">Cap nhat Thang 3 nam 2024</p>
+            {unitLesson?.success && (
+                <Fragment>
+                    <h2 className="font-title text-xl font-bold text-textCustom">{unitLesson.unitLesson.title}</h2>
+                    <p className="text-base text-textCustom">Cập nhật ngày {dayjs(unitLesson.unitLesson.createAt).format('DD/MM/YYYY')}</p>
+                </Fragment>
+            )}
 
             <p className="font-body text-base font-semibold text-red-400 phone:text-sm pm:text-sm">
                 Hãy sử dụng lại kiến thức đã học ở bài trước để giải các câu dưới đây nhé!
             </p>
 
-            {!fillBlankExerciseLoading && fillBlankExerciseData?.fillBlankExercise
-                ? fillBlankExerciseData.fillBlankExercise.questions.map((question: QuestionType, questionIndex: number) => (
-                      <div className="mb-2 flex flex-wrap justify-start gap-2" key={question._id}>
-                          {question.sentence.split('______').map((part: string, index: number) => (
-                              <div key={index} className="flex items-center gap-2">
-                                  <p className="min-w-12 font-body text-base font-medium text-textCustom">{part}</p>
+            <div className="mt-3">
+                {!fillBlankExerciseLoading && fillBlankExerciseData?.fillBlankExercise
+                    ? fillBlankExerciseData.fillBlankExercise.questions.map((question: QuestionType, questionIndex: number) => (
+                          <div className="mb-2 flex flex-wrap justify-start gap-2" key={question._id}>
+                              {question.sentence.split('______').map((part: string, index: number) => (
+                                  <div key={index} className="flex items-center gap-2">
+                                      <p className="min-w-12 font-body text-base font-medium text-textCustom">{part}</p>
 
-                                  {index !== question.sentence.split('______').length - 1 && (
-                                      <Fragment>
-                                          <input
-                                              className={`w-32 rounded border border-slate-400 bg-bgCustom pl-1 font-body font-bold text-textCustom focus:border-blue-400`}
-                                              type="text"
-                                              onChange={(e) => handleSetFillBlankAnswers(e, index, questionIndex)}
-                                          />
-                                          {results ? (
-                                              question?.correctAnswer[index].toLowerCase() ===
-                                                  (answers[questionIndex] as string)?.[index].toLowerCase() ||
-                                              question?.otherAnswer[index].toLowerCase() ===
-                                                  (answers[questionIndex] as string)?.[index].toLowerCase() ? (
-                                                  <Check className="text-green-500" size={18} />
+                                      {index !== question.sentence.split('______').length - 1 && (
+                                          <Fragment>
+                                              <input
+                                                  className={`w-32 rounded border border-slate-400 bg-bgCustom pl-1 font-body font-bold text-textCustom focus:border-blue-400`}
+                                                  type="text"
+                                                  onChange={(e) => handleSetFillBlankAnswers(e, index, questionIndex)}
+                                              />
+                                              {results ? (
+                                                  question?.correctAnswer[index].toLowerCase() ===
+                                                      (answers[questionIndex] as string)?.[index].toLowerCase() ||
+                                                  question?.otherAnswer[index].toLowerCase() ===
+                                                      (answers[questionIndex] as string)?.[index].toLowerCase() ? (
+                                                      <Check className="text-green-500" size={18} />
+                                                  ) : (
+                                                      <X className="text-red-500" size={18} />
+                                                  )
                                               ) : (
-                                                  <X className="text-red-500" size={18} />
-                                              )
-                                          ) : (
-                                              ''
-                                          )}
-                                      </Fragment>
-                                  )}
-                              </div>
-                          ))}
-                      </div>
-                  ))
-                : ''}
+                                                  ''
+                                              )}
+                                          </Fragment>
+                                      )}
+                                  </div>
+                              ))}
+                          </div>
+                      ))
+                    : ''}
+            </div>
 
             {!fillBlankExerciseLoading && fillBlankExerciseData?.success ? (
                 <div className="mt-4 flex items-center justify-center gap-2">
@@ -124,7 +175,12 @@ const FillBlankExerciseCard: React.FC = () => {
                         Kiểm tra
                     </button>
 
-                    <button className={`${allCorrect ? 'btn-success' : 'btn-disabled'} font-body font-medium`}>Bài tiếp theo</button>
+                    <button
+                        className={`${allCorrect ? 'btn-success' : 'btn-disabled'} font-body font-medium`}
+                        onClick={handleNextUnitLesson}
+                    >
+                        Bài tiếp theo
+                    </button>
                 </div>
             ) : (
                 <Empty className="mt-4" />
