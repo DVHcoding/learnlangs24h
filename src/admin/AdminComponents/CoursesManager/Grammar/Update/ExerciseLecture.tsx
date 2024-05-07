3335e993c6d9acb524365edc8a1987ae59b4e388
Đỗ Hùng <dohung2ka3@gmail.com>
2024-05-07 09:56:58 +0700

    refactor: update UI in ExerciseLecture component

--- src/admin/AdminComponents/CoursesManager/Grammar/Update/ExerciseLecture.tsx
+++ src/admin/AdminComponents/CoursesManager/Grammar/Update/ExerciseLecture.tsx
@@ -1,13 +1,25 @@
 // ##########################
 // #      IMPORT NPM        #
 // ##########################
-import { Tabs } from 'antd';
+import { Tabs, Button, message, Popconfirm } from 'antd';
+import { X } from 'lucide-react';
+import type { PopconfirmProps } from 'antd';
 
 // ##########################
 // #    IMPORT Components   #
 // ##########################
 
-const ExerciseLecture:React.FC = () => {
+const ExerciseLecture: React.FC = () => {
+    const confirm: PopconfirmProps['onConfirm'] = (e) => {
+        console.log(e);
+        message.success('Click on Yes');
+    };
+
+    const cancel: PopconfirmProps['onCancel'] = (e) => {
+        console.log(e);
+        message.error('Click on No');
+    };
+
     return (
         <Tabs
             defaultActiveKey="1"
@@ -16,75 +28,130 @@ const ExerciseLecture:React.FC = () => {
                     label: 'Thông tin chung',
                     key: '1',
                     children: (
-                        <div>
-                            <form className="flex flex-col gap-4">
-                                <button className="btn-primary max-w-max">Cập nhật</button>
-                                <div>
-                                    <span className="font-body font-bold">Tên bài học (*)</span>
-                                    <input
-                                        type="text"
-                                        className="text-segoe mt-1 block w-[21.8rem] rounded-[3px] border border-gray-300 p-1 focus:border-blue-400
+                        <form className="flex flex-col gap-4">
+                            <button className="btn-primary max-w-max">Cập nhật</button>
+                            <div>
+                                <span className="font-body font-bold">Tên bài học (*)</span>
+                                <input
+                                    type="text"
+                                    className="text-segoe mt-1 block w-[21.8rem] rounded-[3px] border border-gray-300 p-1 focus:border-blue-400
                                         sm:w-full"
-                                    />
-                                </div>
+                                />
+                            </div>
 
-                                <div>
-                                    <span className="font-body font-bold">Thời gian (*)</span>
-                                    <input
-                                        type="text"
-                                        className="text-segoe mt-1 block w-[21.8rem] rounded-[3px] border border-gray-300 p-1 focus:border-blue-400
+                            <div>
+                                <span className="font-body font-bold">Thời gian (*)</span>
+                                <input
+                                    type="text"
+                                    className="text-segoe mt-1 block w-[21.8rem] rounded-[3px] border border-gray-300 p-1 focus:border-blue-400
                                         sm:w-full"
-                                    />
-                                </div>
+                                />
+                            </div>
 
-                                <div>
-                                    <span className="font-body font-bold">Ngày tạo (*)</span>
-                                    <input
-                                        type="text"
-                                        className="text-segoe mt-1 block w-[21.8rem] rounded-[3px] border border-gray-300 p-1 focus:border-blue-400
+                            <div>
+                                <span className="font-body font-bold">Ngày tạo (*)</span>
+                                <input
+                                    type="text"
+                                    className="text-segoe mt-1 block w-[21.8rem] rounded-[3px] border border-gray-300 p-1 focus:border-blue-400
                                         sm:w-full"
-                                    />
-                                </div>
+                                />
+                            </div>
 
-                                <div>
-                                    <span className="font-body font-bold">Loại bài tập (*)</span>
-                                    <select
-                                        id="small"
-                                        className="mt-2 block w-[21.8rem] rounded-[3px] border border-gray-300 bg-white 
+                            <div>
+                                <span className="font-body font-bold">Loại bài học (*)</span>
+                                <select
+                                    id="small"
+                                    className="mt-2 block w-[21.8rem] rounded-[3px] border border-gray-300 bg-white 
                                         p-2 text-sm focus:border-blue-500 focus:ring-blue-500 sm:w-full"
-                                    >
-                                        <option value="FillBlank" selected>
-                                            Điền vào chỗ trống
-                                        </option>
-                                    </select>
-                                </div>
+                                >
+                                    <option value="FillBlank" selected>
+                                        Thực hành
+                                    </option>
+                                </select>
+                            </div>
 
-                                <div>
-                                    <span className="font-body font-bold">Chương học (*)</span>
-                                    <select
-                                        id="small"
-                                        className="mt-2 block w-[21.8rem] rounded-[3px] border border-gray-300 bg-white 
+                            <div>
+                                <span className="font-body font-bold">Loại bài tập (*)</span>
+                                <select
+                                    id="small"
+                                    className="mt-2 block w-[21.8rem] rounded-[3px] border border-gray-300 bg-white 
                                                       p-2 text-sm focus:border-blue-500 focus:ring-blue-500 sm:w-full"
-                                    >
-                                        <option className="hidden"> --- Chọn chương --- </option>
-                                        <option value="US">United States</option>
-                                        <option value="CA">Canada</option>
-                                        <option value="FR">France</option>
-                                        <option value="DE">Germany</option>
-                                    </select>
-                                </div>
-                            </form>
-                        </div>
+                                >
+                                    <option selected>Điền vào chỗ trống</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <span className="font-body font-bold">Chương học (*)</span>
+                                <select
+                                    id="small"
+                                    className="mt-2 block w-[21.8rem] rounded-[3px] border border-gray-300 bg-white 
+                                                      p-2 text-sm focus:border-blue-500 focus:ring-blue-500 sm:w-full"
+                                >
+                                    <option className="hidden"> --- Chọn chương --- </option>
+                                    <option value="US">United States</option>
+                                    <option value="CA">Canada</option>
+                                    <option value="FR">France</option>
+                                    <option value="DE">Germany</option>
+                                </select>
+                            </div>
+                        </form>
                     ),
                 },
                 {
                     label: 'Câu hỏi',
                     key: '2',
                     children: (
-                        <div>
+                        <div className="flex flex-col gap-4">
                             <div>
-                                <span className="font-body font-bold">Mô tả (*)</span>
+                                <Button type="primary">Thêm câu hỏi</Button>
                             </div>
+
+                            <ul className="flex flex-wrap items-center gap-2">
+                                {[...Array(20)].map((_item, index: number) => (
+                                    <li key={index} className="relative grow border-2 border-dotted p-4">
+                                        <Popconfirm
+                                            title="Delete the task"
+                                            description="Are you sure to delete this task?"
+                                            onConfirm={confirm}
+                                            onCancel={cancel}
+                                            okText="Yes"
+                                            cancelText="No"
+                                        >
+                                            <X className="absolute right-1 top-1 cursor-pointer text-red-500" size={19} />
+                                        </Popconfirm>
+
+                                        <div>
+                                            <div>
+                                                <span className="select-none font-body font-bold">Câu hỏi (*)</span>
+                                                <input
+                                                    type="text"
+                                                    className="text-segoe mt-1 block w-full rounded-[3px] border border-gray-300 p-1
+                                                focus:border-blue-400"
+                                                />
+                                            </div>
+
+                                            <div>
+                                                <span className="select-none font-body font-bold">Đáp án (*)</span>
+                                                <input
+                                                    type="text"
+                                                    className="text-segoe mt-1 block w-full rounded-[3px] border border-gray-300 p-1
+                                                focus:border-blue-400"
+                                                />
+                                            </div>
+
+                                            <div>
+                                                <span className="select-none font-body font-bold">Đáp án khác (*)</span>
+                                                <input
+                                                    type="text"
+                                                    className="text-segoe mt-1 block w-full rounded-[3px] border border-gray-300
+                                                p-1 focus:border-blue-400"
+                                                />
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     ),
                 },
