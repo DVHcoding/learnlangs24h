d0c69cf16b815b4276e122e599f0a751c6dd129b
Đỗ Hùng <dohung2ka3@gmail.com>
2024-06-01 17:43:55 +0700

    feat(Messenger.tsx): make a new component

---
+++ src/components/Messenger/Messenger.tsx
@@ -0,0 +1,13 @@
+// ##########################
+// #      IMPORT NPM        #
+// ##########################
+
+// ##########################
+// #    IMPORT Components   #
+// ##########################
+
+const Messenger = () => {
+    return <div>Messenger</div>;
+};
+
+export default Messenger;
