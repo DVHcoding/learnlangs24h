8cc659efd7494e004aa10c5d3dfdb569f78a6765
Đỗ Hùng <dohung2ka3@gmail.com>
2024-06-10 14:59:31 +0700

    feat(socket.tsx): new component

---
+++ src/utils/socket.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useMemo, useContext, ReactNode } from 'react';
+import io, { Socket } from 'socket.io-client';
+
+interface SocketContextType {
+    socket: Socket | null;
+}
+
+const SocketContext = createContext<SocketContextType | undefined>(undefined);
+
+const useSocket = (): SocketContextType => {
+    const context = useContext(SocketContext);
+    if (!context) {
+        throw new Error('useSocket must be used within a SocketProvider');
+    }
+    return context;
+};
+
+interface SocketProviderProps {
+    children: ReactNode;
+}
+
+const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+    const socket = useMemo(() => io('http://localhost:4000', { withCredentials: true }), []);
+
+    return <SocketContext.Provider value={{ socket }}>{children}</SocketContext.Provider>;
+};
+
+export { SocketProvider, useSocket };
