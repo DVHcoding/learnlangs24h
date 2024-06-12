ffa79272d0be30b162a7aaab8351ed81d3a1c939
Đỗ Hùng <dohung2ka3@gmail.com>
2024-06-13 03:31:32 +0700

    chore(*): wrap div on Tippy

--- src/components/Tippys/TippyNotify.tsx
+++ src/components/Tippys/TippyNotify.tsx
@@ -17,70 +17,72 @@ const TippyNotify: React.FC = () => {
     const hide = () => setVisible(false);
 
     return (
-        <TippyProvider
-            visible={visible}
-            placement="bottom-end"
-            onClickOutside={hide}
-            content={
-                <div className="rounded-md border border-bdCustom bg-bgCustom shadow">
-                    <div className="flex items-center justify-between gap-2 p-4">
-                        <h3 className="font-body font-bold text-textCustom lg:text-lg">Thông báo</h3>
+        <div>
+            <TippyProvider
+                visible={visible}
+                placement="bottom-end"
+                onClickOutside={hide}
+                content={
+                    <div className="rounded-md border border-bdCustom bg-bgCustom shadow">
+                        <div className="flex items-center justify-between gap-2 p-4">
+                            <h3 className="font-body font-bold text-textCustom lg:text-lg">Thông báo</h3>
 
-                        <button
-                            className="rounded-md p-1 font-body font-semibold text-orange-600
-                                    transition-all duration-200 hover:bg-bgHoverGrayDark"
-                        >
-                            Đánh dấu đã đọc
-                        </button>
-                    </div>
+                            <button
+                                className="rounded-md p-1 font-body font-semibold text-orange-600
+                                        transition-all duration-200 hover:bg-bgHoverGrayDark"
+                            >
+                                Đánh dấu đã đọc
+                            </button>
+                        </div>
 
-                    <ul className="scrollbar flex h-96 flex-col gap-4 overflow-auto px-2">
-                        {Array.from(new Array(7)).map((_, index) => (
-                            <Link to={`/${index}`} key={index} style={{ textDecoration: 'none' }}>
-                                <li
-                                    className="flex cursor-pointer items-center justify-between gap-2 rounded-lg
-                                            p-2 transition-all duration-100 hover:bg-bgHoverGrayDark"
-                                >
-                                    <Avatar size="md" circle src="https://avatars.githubusercontent.com/u/12592949" alt="@superman66" />
+                        <ul className="scrollbar flex h-96 flex-col gap-4 overflow-auto px-2">
+                            {Array.from(new Array(7)).map((_, index) => (
+                                <Link to={`/${index}`} key={index} style={{ textDecoration: 'none' }}>
+                                    <li
+                                        className="flex cursor-pointer items-center justify-between gap-2 rounded-lg
+                                                p-2 transition-all duration-100 hover:bg-bgHoverGrayDark"
+                                    >
+                                        <Avatar size="md" circle src="https://avatars.githubusercontent.com/u/12592949" alt="@superman66" />
 
-                                    <div>
-                                        <h4 className="font-body leading-6 text-textCustom">
-                                            <span className="font-semibold">Hoang anh</span> đã thích bình luận của bạn
-                                        </h4>
-                                        <p className="font-body font-semibold text-lime-600">4 tháng trước</p>
-                                    </div>
-                                </li>
+                                        <div>
+                                            <h4 className="font-body leading-6 text-textCustom">
+                                                <span className="font-semibold">Hoang anh</span> đã thích bình luận của bạn
+                                            </h4>
+                                            <p className="font-body font-semibold text-lime-600">4 tháng trước</p>
+                                        </div>
+                                    </li>
+                                </Link>
+                            ))}
+                        </ul>
+
+                        <div className="py-3 transition-all duration-200 hover:bg-bgHoverGrayDark">
+                            <Link
+                                to="/notification"
+                                className="font-body font-bold text-orange-600 hover:text-orange-600"
+                                style={{ textDecoration: 'none' }}
+                            >
+                                <button className="mx-auto">Xem tất cả thông báo</button>
                             </Link>
-                        ))}
-                    </ul>
-
-                    <div className="py-3 transition-all duration-200 hover:bg-bgHoverGrayDark">
-                        <Link
-                            to="/notification"
-                            className="font-body font-bold text-orange-600 hover:text-orange-600"
-                            style={{ textDecoration: 'none' }}
-                        >
-                            <button className="mx-auto">Xem tất cả thông báo</button>
-                        </Link>
+                        </div>
                     </div>
-                </div>
-            }
-        >
-            <Badge
-                onClick={() => setVisible(!visible)}
-                content={5}
-                role="button" // Adding role attribute to indicate clickable element
-                aria-label="Toggle visibility" // Adding aria-label for accessibility
-                className="cursor-pointer select-none"
+                }
             >
-                <BellRing
-                    size={22}
-                    strokeWidth={1.6}
-                    className="text-textCustom"
-                    aria-hidden="true" // Adding aria-hidden to hide from accessibility tree as it's decorative
-                />
-            </Badge>
-        </TippyProvider>
+                <Badge
+                    onClick={() => setVisible(!visible)}
+                    content={5}
+                    role="button" // Adding role attribute to indicate clickable element
+                    aria-label="Toggle visibility" // Adding aria-label for accessibility
+                    className="cursor-pointer select-none"
+                >
+                    <BellRing
+                        size={22}
+                        strokeWidth={1.6}
+                        className="text-textCustom"
+                        aria-hidden="true" // Adding aria-hidden to hide from accessibility tree as it's decorative
+                    />
+                </Badge>
+            </TippyProvider>
+        </div>
     );
 };
 
--- src/components/Tippys/TippyProfile.tsx
+++ src/components/Tippys/TippyProfile.tsx
@@ -32,90 +32,92 @@ const TippyProfile: React.FC = () => {
     };
 
     return (
-        <TippyProvider
-            hideOnClick={false}
-            content={
-                <div className="min-w-44 rounded-md border border-bdCustom bg-bgCustom p-4 shadow">
-                    <div className="flex items-center gap-2">
-                        <Avatar src={<img src={data?.success ? `${data.user?.photo?.url}` : DefaultAvatar} alt="avatar" />} />
-                        <div>
-                            <p className="font-body font-bold text-textCustom">
-                                {data?.user?.username}
+        <div>
+            <TippyProvider
+                hideOnClick={false}
+                content={
+                    <div className="min-w-44 rounded-md border border-bdCustom bg-bgCustom p-4 shadow">
+                        <div className="flex items-center gap-2">
+                            <Avatar src={<img src={data?.success ? `${data.user?.photo?.url}` : DefaultAvatar} alt="avatar" />} />
+                            <div>
+                                <p className="font-body font-bold text-textCustom">
+                                    {data?.user?.username}
 
-                                {!data?.success && 'Anonymous'}
-                            </p>
-                            <p className="m-0 font-body text-xs text-textCustom">{data?.user?.email}</p>
+                                    {!data?.success && 'Anonymous'}
+                                </p>
+                                <p className="m-0 font-body text-xs text-textCustom">{data?.user?.email}</p>
+                            </div>
                         </div>
+
+                        <div className="my-2 h-[0.5px] w-full bg-slate-200"></div>
+
+                        {data?.success && (
+                            <div>
+                                <Link
+                                    to={`/profile/${data?.user?.nickname}`}
+                                    className="text-textCustom transition-all 
+                                        duration-200 hover:text-orange-400 hover:text-textCustom"
+                                    style={{ textDecoration: 'none' }}
+                                >
+                                    <button aria-label="btn_profile" className="font-body">
+                                        Trang cá nhân
+                                    </button>
+                                </Link>
+
+                                <div className="my-2 h-[0.5px] w-full bg-slate-200"></div>
+
+                                <Link
+                                    to="/blog"
+                                    className="text-textCustom transition-all 
+                                        duration-200 hover:text-orange-400 hover:text-textCustom"
+                                    style={{ textDecoration: 'none' }}
+                                >
+                                    <button aria-label="btn_writeBlog" className="font-body">
+                                        Viết blog
+                                    </button>
+                                </Link>
+
+                                <Link
+                                    to="/myPost"
+                                    className="text-textCustom transition-all 
+                                        duration-200 hover:text-orange-400 hover:text-textCustom"
+                                    style={{ textDecoration: 'none' }}
+                                >
+                                    <button aria-label="btn_myBlog" className="mt-2 font-body">
+                                        Bài viết của tôi
+                                    </button>
+                                </Link>
+
+                                <div className="my-2 h-[0.5px] w-full bg-slate-200"></div>
+                            </div>
+                        )}
+
+                        {data?.success ? (
+                            <button
+                                disabled={isLoading}
+                                onClick={logoutHandler}
+                                aria-label="btn_logout"
+                                className="font-body font-normal text-textCustom 
+                                        transition-all duration-200 hover:text-red-500"
+                            >
+                                Đăng xuất
+                            </button>
+                        ) : (
+                            <Link
+                                to="/login"
+                                className="font-body font-normal text-textCustom 
+                                        transition-all duration-200 hover:text-green-500"
+                                style={{ textDecoration: 'none' }}
+                            >
+                                Đăng nhập
+                            </Link>
+                        )}
                     </div>
-
-                    <div className="my-2 h-[0.5px] w-full bg-slate-200"></div>
-
-                    {data?.success && (
-                        <div>
-                            <Link
-                                to={`/profile/${data?.user?.nickname}`}
-                                className="text-textCustom transition-all 
-                                    duration-200 hover:text-orange-400 hover:text-textCustom"
-                                style={{ textDecoration: 'none' }}
-                            >
-                                <button aria-label="btn_profile" className="font-body">
-                                    Trang cá nhân
-                                </button>
-                            </Link>
-
-                            <div className="my-2 h-[0.5px] w-full bg-slate-200"></div>
-
-                            <Link
-                                to="/blog"
-                                className="text-textCustom transition-all 
-                                    duration-200 hover:text-orange-400 hover:text-textCustom"
-                                style={{ textDecoration: 'none' }}
-                            >
-                                <button aria-label="btn_writeBlog" className="font-body">
-                                    Viết blog
-                                </button>
-                            </Link>
-
-                            <Link
-                                to="/myPost"
-                                className="text-textCustom transition-all 
-                                    duration-200 hover:text-orange-400 hover:text-textCustom"
-                                style={{ textDecoration: 'none' }}
-                            >
-                                <button aria-label="btn_myBlog" className="mt-2 font-body">
-                                    Bài viết của tôi
-                                </button>
-                            </Link>
-
-                            <div className="my-2 h-[0.5px] w-full bg-slate-200"></div>
-                        </div>
-                    )}
-
-                    {data?.success ? (
-                        <button
-                            disabled={isLoading}
-                            onClick={logoutHandler}
-                            aria-label="btn_logout"
-                            className="font-body font-normal text-textCustom 
-                                    transition-all duration-200 hover:text-red-500"
-                        >
-                            Đăng xuất
-                        </button>
-                    ) : (
-                        <Link
-                            to="/login"
-                            className="font-body font-normal text-textCustom 
-                                    transition-all duration-200 hover:text-green-500"
-                            style={{ textDecoration: 'none' }}
-                        >
-                            Đăng nhập
-                        </Link>
-                    )}
-                </div>
-            }
-        >
-            <Avatar size={35} src={<img src={data?.success ? `${data.user?.photo?.url}` : DefaultAvatar} alt="avatar" />} />
-        </TippyProvider>
+                }
+            >
+                <Avatar size={35} src={<img src={data?.success ? `${data.user?.photo?.url}` : DefaultAvatar} alt="avatar" />} />
+            </TippyProvider>
+        </div>
     );
 };
 
