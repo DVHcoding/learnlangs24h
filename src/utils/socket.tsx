4d37483888877f809ce3c72f96ac1175e3c1f868
Đỗ Hùng <dohung2ka3@gmail.com>
2024-06-13 18:04:53 +0700

    refactor(socket.tsx): shorten the code

--- src/utils/socket.tsx
+++ src/utils/socket.tsx
@@ -1,28 +1,29 @@
-import React, { createContext, useMemo, useContext, ReactNode } from 'react';
+// ##########################
+// #      IMPORT NPM        #
+// ##########################
+import { createContext, useMemo, useContext, ReactNode } from 'react';
 import io, { Socket } from 'socket.io-client';
 
-interface SocketContextType {
-    socket: Socket | null;
-}
-// Tạo một context mới để lưu dữ liệu
-const SocketContext = createContext<SocketContextType | undefined>(undefined);
+// ##########################
+// #    IMPORT Components   #
+// ##########################
+const SocketContext = createContext<Socket | null>(null);
 
-const useSocket = (): SocketContextType => {
+//Sử dụng useContext
+const useSocket = (): Socket => {
     const context = useContext(SocketContext);
+
     if (!context) {
         throw new Error('useSocket must be used within a SocketProvider');
     }
     return context;
 };
 
-interface SocketProviderProps {
-    children: ReactNode;
-}
-
-const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+// Tạo contextProvider
+const SocketProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const socket = useMemo(() => io('http://localhost:4000', { withCredentials: true }), []);
 
-    return <SocketContext.Provider value={{ socket }}>{children}</SocketContext.Provider>;
+    return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
 };
 
 export { SocketProvider, useSocket };
