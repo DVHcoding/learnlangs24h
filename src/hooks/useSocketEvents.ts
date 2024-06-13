f3a9b827279fbf2e8bfc6933fd8b952dc4459124
Đỗ Hùng <dohung2ka3@gmail.com>
2024-06-13 23:31:44 +0700

    chore(useSocketEvents.ts): comment somethings

--- src/hooks/useSocketEvents.ts
+++ src/hooks/useSocketEvents.ts
@@ -4,9 +4,26 @@
 import { useEffect } from 'react';
 import { Socket } from 'socket.io-client';
 
-// ##########################
-// #    IMPORT Components   #
-// ##########################
+/**
+ * Giả sử bạn có một đối tượng đơn giản như sau:
+ *
+ * const person = {
+ *   name: "John",
+ *   age: 30,
+ *   city: "New York"
+ * };
+ *
+ * Bạn có thể sử dụng Object.entries để lấy các cặp [key, value] từ đối tượng person:
+ * const entries = Object.entries(person);
+ * console.log(entries);
+ *
+ * Kết quả sẽ là một mảng chứa các mảng con, mỗi mảng con là một cặp [key, value]:
+ * [
+ *   ["name", "John"],
+ *   ["age", 30],
+ *   ["city", "New York"]
+ * ]
+ */
 
 interface Handlers {
     [key: string]: (...args: any[]) => void;
@@ -15,6 +32,15 @@ interface Handlers {
 const useSocketEvents = (socket: Socket, handlers: Handlers) => {
     useEffect(() => {
         Object.entries(handlers).forEach(([event, handler]) => {
+            /**
+             * Lắng nghe sự kiện và nhận dữ liệu từ server khi sự kiện đó xảy ra.
+             * Khi sự kiện được phát từ server, hàm handler sẽ được gọi với dữ liệu nhận được.
+             *
+             * Hoặc ta có thể làm trực tiếp như ví dụ dưới đây (Không khuyến khích vì không tái sử dụng được handler):
+             * socket.on(event, (data) => {
+             *    console.log(data);
+             * });
+             */
             socket.on(event, handler);
         });
 
