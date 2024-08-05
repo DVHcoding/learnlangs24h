167e2292a54c04cbcfeb4578154310c2c21a517a
DVHcoding <dohung2ka3@gmail.com>
2024-08-05 23:58:34 +0700

    feat(ErrorFallback.tsx): using Lottie animation

--- src/components/ErrorFallback/ErrorFallblack.tsx
+++ src/components/ErrorFallback/ErrorFallblack.tsx
@@ -3,20 +3,26 @@
 // ##########################################################################
 import { Alert, Button } from 'antd';
 import { FallbackProps } from 'react-error-boundary';
+import Lottie from 'lottie-react';
 
 // ##########################################################################
 // #                           IMPORT Components                            #
 // ##########################################################################
+import ErrorAnimation from '@assets/animations/error_animation.json';
 
 const ErrorFallback: React.FC<FallbackProps> = ({ error }) => {
     return (
         <div className="h-screen bg-bgCustomCard p-8">
             <div
-                className="mx-auto flex h-full max-w-max flex-col gap-4 rounded-md
+                className="mx-auto flex h-full max-w-max flex-col items-center gap-4 rounded-md
                 bg-bgCustomCardItem p-8"
             >
                 <p className="font-body text-lg font-bold">Đã có lỗi xảy ra. Admin sẽ sớm khắc phục điều này!</p>
                 <Alert message={`${error.message}`} type="error" />
+                <div className="w-[80%]">
+                    <Lottie animationData={ErrorAnimation} loop={true} />
+                </div>
+
                 <Button type="primary" className="max-w-max" onClick={() => (window.location.href = '/')}>
                     Về trang chủ
                 </Button>
