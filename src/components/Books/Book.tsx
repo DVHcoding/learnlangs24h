9a68778cc12cc9ca1f8f5f31b0ac79af94ecf9aa
DVHcoding <dohung2ka3@gmail.com>
2024-08-08 16:55:04 +0700

    feat(*): handle render bookCategory data

--- src/components/Books/Book.tsx
+++ src/components/Books/Book.tsx
@@ -1,8 +1,8 @@
 // ##########################################################################
 // #                                 IMPORT NPM                             #
 // ##########################################################################
-import React, { Fragment, useState, useEffect } from 'react';
-import { Badge, Breadcrumb, Button, Pagination, Select } from 'antd';
+import React, { Fragment, useState } from 'react';
+import { Badge, Breadcrumb, Button, Empty, Pagination, Select } from 'antd';
 import { Link } from 'react-router-dom';
 const { Option } = Select;
 
@@ -31,7 +31,7 @@ const Book: React.FC = () => {
     /* ########################################################################## */
     /*                                     RTK                                    */
     /* ########################################################################## */
-    const { data: booksData, refetch } = useGetBooksQuery(
+    const { data: booksData } = useGetBooksQuery(
         { page: currentPage, limit: itemsPerPage, bookCategory },
         { skip: !currentPage || !itemsPerPage || !bookCategory }
     );
@@ -61,84 +61,85 @@ const Book: React.FC = () => {
     /* ########################################################################## */
     /*                                  useEffect                                 */
     /* ########################################################################## */
-    useEffect(() => {
-        refetch();
-    }, [currentPage, itemsPerPage]);
 
     return (
-        <div className="h-full px-4 phone:p-1">
+        <div className="overflow-hidden pl-4 phone:p-1" style={{ height: 'calc(100% - 3.8rem)' }}>
             {/* Breadcrumbs */}
             <div className="mb-2 flex justify-between">
                 <Breadcrumb items={[{ title: <Link to="/">Home</Link> }, { title: 'Books' }]} />
             </div>
 
-            <div>
-                <div className="mb-4 flex flex-wrap items-center gap-4">
-                    <div className="flex items-center gap-2">
-                        <p className="text-textCustom">Sắp xếp theo:</p>
-                        <Select
-                            defaultValue="all"
-                            style={{ width: 150 }}
-                            className="text-textCustom"
-                            onChange={(value: string) => setBookCategory(value)}
-                        >
-                            <Option value="all">All Categories</Option>
-                            {bookCategoriesData?.bookCategories?.map((bookCategory: BookCategory) => (
-                                <Option value={bookCategory._id} key={bookCategory._id}>
-                                    {bookCategory.name}
-                                </Option>
-                            ))}
-                        </Select>
-                    </div>
-
-                    <div className="flex items-center gap-2">
-                        <p className="text-textCustom">Hiển thị:</p>
-                        <Select defaultValue="12" style={{ width: 70 }} className="text-textCustom" onChange={handleItemsPerPageChange}>
-                            <Option value="12">12</Option>
-                            <Option value="24">24</Option>
-                        </Select>
-                    </div>
+            <div className="mb-4 flex flex-wrap items-center gap-4">
+                <div className="flex items-center gap-2">
+                    <p className="text-textCustom">Sắp xếp theo:</p>
+                    <Select
+                        defaultValue="all"
+                        style={{ width: 150 }}
+                        className="text-textCustom"
+                        onChange={(value: string) => setBookCategory(value)}
+                    >
+                        <Option value="all">All Categories</Option>
+                        {bookCategoriesData?.bookCategories?.map((bookCategory: BookCategory) => (
+                            <Option value={bookCategory._id} key={bookCategory._id}>
+                                {bookCategory.name}
+                            </Option>
+                        ))}
+                    </Select>
                 </div>
 
+                <div className="flex items-center gap-2">
+                    <p className="text-textCustom">Hiển thị:</p>
+                    <Select defaultValue="12" style={{ width: 70 }} className="text-textCustom" onChange={handleItemsPerPageChange}>
+                        <Option value="12">12</Option>
+                        <Option value="24">24</Option>
+                    </Select>
+                </div>
+            </div>
+
+            <div className="scrollbar overflow-auto" style={{ height: 'calc(100% - 4.8rem' }}>
                 {/* Books */}
-                <div className="bg-bgCustomCard p-2 pb-10">
-                    <ul
-                        className="grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 
-                        phone:grid-cols-1"
-                    >
-                        {booksData?.books?.map((book: BookTypes) => (
-                            <Fragment key={book._id}>
-                                <Badge.Ribbon text="Premium" color="orange" className={`${book.premium ? 'block' : 'hidden'}`}>
-                                    <li
-                                        className="flex min-h-[26.688rem] flex-col items-center
+                <div className="min-h-full bg-bgCustomCard p-2 pb-4">
+                    {booksData?.books.length ? (
+                        <ul
+                            className="grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 
+                            phone:grid-cols-1"
+                        >
+                            {booksData?.books?.map((book: BookTypes) => (
+                                <Fragment key={book._id}>
+                                    <Badge.Ribbon text="Premium" color="orange" className={`${book.premium ? 'block' : 'hidden'}`}>
+                                        <li
+                                            className="flex min-h-[26.688rem] flex-col items-center
                                         rounded-md bg-bgCustomCardItem p-4"
-                                    >
-                                        <img
-                                            src={book?.photo?.url}
-                                            alt="book"
-                                            className="max-h-[300px] min-w-full select-none rounded-md object-cover"
-                                        />
-                                        <p
-                                            className="mt-2 line-clamp-2 cursor-default font-be text-base font-medium 
-                                            text-textCustomProcess transition-all hover:text-textCustomProcess"
                                         >
-                                            {book.name}
-                                        </p>
+                                            <img
+                                                src={book?.photo?.url}
+                                                alt="book"
+                                                className="max-h-[300px] min-w-full select-none rounded-md object-cover"
+                                            />
+                                            <p
+                                                className="mt-2 line-clamp-2 cursor-default font-be text-base font-medium 
+                                            text-textCustomProcess transition-all hover:text-textCustomProcess"
+                                            >
+                                                {book.name}
+                                            </p>
 
-                                        <div className="mt-auto flex max-w-max flex-wrap justify-center gap-2">
-                                            <Button type="primary">Read</Button>
-                                            <Button type="primary">View PDF</Button>
-                                            <Button disabled={book.premium} type="dashed">
-                                                Download
-                                            </Button>
-                                        </div>
-                                    </li>
-                                </Badge.Ribbon>
-                            </Fragment>
-                        ))}
-                    </ul>
+                                            <div className="mt-auto flex max-w-max flex-wrap justify-center gap-2">
+                                                <Button type="primary">Read</Button>
+                                                <Button type="primary">View PDF</Button>
+                                                <Button disabled={book.premium} type="dashed">
+                                                    Download
+                                                </Button>
+                                            </div>
+                                        </li>
+                                    </Badge.Ribbon>
+                                </Fragment>
+                            ))}
+                        </ul>
+                    ) : (
+                        <Empty description="No books found" />
+                    )}
 
-                    <div className="mx-auto mt-4 max-w-max">
+                    <div className={`mx-auto mt-4 max-w-max ${booksData?.books.length ? 'block' : 'hidden'}`}>
                         <Pagination current={currentPage} total={totalItems} pageSize={itemsPerPage} onChange={handlePageChange} />
                     </div>
                 </div>
